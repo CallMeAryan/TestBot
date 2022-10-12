5ad2ca5cb705122171aa97c19ba0265e7bb60c52
Liven <tiwariaryan035@gmail.com>
2022-10-12 08:02:40 +0530

    Update DiscordBot.java

--- src/main/java/net/liven/bot/DiscordBot.java
+++ src/main/java/net/liven/bot/DiscordBot.java
@@ -30,61 +30,16 @@ public class DiscordBot {
     public static String Accepted_Application_Channel_ID = "917017063866900491";
     public static HashMap<String, String> HM_Running_applications = new HashMap<>();
     public static List<String> LS_Running_applications = new ArrayList<>();
-//    static Jedis jedis;
-
 
 
     public static void main(String[] args) throws LoginException {
 
         DbFunctions db = new DbFunctions();
-//        Connection conn = db.connect_to_db("jellydb","liven","liven12");
-
-        String ip = "178.32.120.94";
-        int port = 3306;
-        String dbname = "test";
-        String user = "mcgo";
-        String pass = "dBzRWFq2VsJFf";
-
-        String url = "jdbc:mysql://" + ip + ":" + port +"/" + dbname;
-
-        System.out.println(url);
-
-        Connection conn = null;
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-//            String url = "jdbc:mysql://" + "178.32.120.94" + ":" + "3306" + "/" + dbname;
-
-
-//            String url = "jdbc:mysql://" + ip + ":" + port + "/" + dbname;
-            conn = DriverManager.getConnection(url, user, pass);
-            if (conn != null) {
-                System.out.println("Connection Established");
-
-            } else {
-                System.out.println("Connection Failed");
-            }
-
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-
-        System.out.println(db.getNameColor(conn, "TEST-oubsiuaiugfiagiyvsdiygawiu"));
-//        jedis = new Jedis("localhost", 6379);
-//        jedis.auth("liven@12");
-//
-//        jedis.publish("test", "message right");
-//
-//        JedisManager jm = new JedisManager(jedis, "test");
-//
-//        jm.broadcastMessage("testmessage2");
-
-//        JDA jda = JDABuilder.createDefault("ODY0MzcwNDg2OTQ2MDM3ODEw.Gl4_k3.x_p-00U5P5vqrp5ZkaMFqQaWPujCfs0Tx3y8Bc", GatewayIntent.GUILD_MESSAGES, GatewayIntent.GUILD_MEMBERS)
-//                .setActivity(Activity.playing("learning stuff"))
-//                .addEventListeners(new BotCommands(db ,conn, jedis, jm.getListener(), jm))
-//                .addEventListeners(new Tasks())
-//                .build();
-
-//        jedis.subscribe(jm.getListener(), "test");
+        JDA jda = JDABuilder.createDefault("****************************", GatewayIntent.GUILD_MESSAGES, GatewayIntent.GUILD_MEMBERS)
+                .setActivity(Activity.playing("learning stuff"))
+                .addEventListeners(new BotCommands(db ,conn, jedis, jm.getListener(), jm))
+                .addEventListeners(new Tasks())
+                .build();
 
     }
 
